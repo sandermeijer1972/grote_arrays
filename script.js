b03d106c5ea22c344d9cf66b07b4288fd8918498
Sander Meijer <sandertje1972@hotmail.com>
2021-05-12 18:28:19 +0200

    added meeste-mensen

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const emptyResults = () => {
     resultOfButtonClick.classList.remove("steenbokvrouwen");
     resultOfButtonClick.classList.remove("creditcards");
     resultOfButtonClick.classList.remove("gemiddelde");
+    resultOfButtonClick.classList.remove("meestemensen");
 
 };
 
@@ -149,10 +150,45 @@ addOudeCreditcardsToDOM.addEventListener("click", addCreditcardsToDOM);
 
 //MEESTE MENSEN
 
+const getCountOfCountries = []; 
+const makeNewArray = allCountriesOnlyOnce.forEach(country => {
+    const peopleInOneCountry = randomPersonData.filter(person => person.region == country);
+    getCountOfCountries.push(country + "/" + peopleInOneCountry.length);
+});
+const countriesSortedOnCount = getCountOfCountries.slice().sort((a, b) => (parseInt(a.split("/")[1]) > parseInt(b.split("/")[1])) ? -1 : 1);
+console.log(countriesSortedOnCount);
 
+const addListOfMostPeople = (array) => {
+    const newTopLi = document.createElement('li');
+    const newTopLeftP = document.createElement('p');
+    const newTopRightDiv = document.createElement('div');
+    newTopLeftP.innerText = "land";    
+    newTopRightDiv.innerText = "aantal mensen";    
+    newTopLi.classList.add("top");
+    newTopLi.appendChild(newTopLeftP);
+    newTopLi.appendChild(newTopRightDiv);
+    resultOfButtonClick.appendChild(newTopLi);
+    array.forEach(country => {
+        const newLi = document.createElement('li');
+        const newP = document.createElement('p');
+        newP.innerText = (country.split("/")[0]);
+        const newDiv = document.createElement('div');
+        newDiv.innerText = (country.split("/")[1]);
+        newLi.appendChild(newP);
+        newLi.appendChild(newDiv);
+        resultOfButtonClick.appendChild(newLi);
+    });
+};
+
+const addMostPeopleToDOM = () => {
+    emptyResults();
+    uitlegButton.innerHTML = "Het aantal mensen uit de database per land gesorteerd:";
+    resultOfButtonClick.classList.add("meestemensen");
+    addListOfMostPeople(countriesSortedOnCount);
+};
 
 const addMeesteMensenToDOM = document.getElementById('meestemensen');
-addMeesteMensenToDOM.addEventListener("click", workInProgress);
+addMeesteMensenToDOM.addEventListener("click", addMostPeopleToDOM);
 
 
 //GEMIDDELDE LEEFTIJD
